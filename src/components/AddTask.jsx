e2b3d1c4b44858e3193f92b449a8d1154864001f
mariana-nogueira21 <mariana.nogueira21@gmail.com>
2025-09-21 19:18:44 -0300

    iniciar projeto

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function AddTask({onAddTaskSubmit}) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow-md flex flex-col">
+      <input 
+        type="text" 
+        placeholder="Digite o título da tarefa" 
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input 
+        type="text" 
+        placeholder="Digite a descrição da tarefa" 
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button 
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+        onClick={() => onAddTaskSubmit(title, description)}
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
